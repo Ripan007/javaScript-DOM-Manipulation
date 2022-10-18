1d70a0a9f5650c059b53bf409bdf34d9ec5f7ec0
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-19 00:18:15 +0530

    add

---
+++ script.js
@@ -0,0 +1,19 @@
+// const title = document.getElementById("main-heading");
+// console.log(title);
+
+// const list = document.getElementsByClassName("list-items");
+// console.log(list);
+
+// const lists = document.getElementsByTagName("li");
+// console.log(lists);
+
+// const heading = document.querySelector("div");
+// console.log(heading);
+
+// const container = document.querySelectorAll("div");
+// console.log(container);
+
+// const heading = document.querySelector("#main-heading");
+// heading.style.color = "blue";
+
+
