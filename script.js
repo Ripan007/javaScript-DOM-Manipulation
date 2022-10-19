ef87c9d3eb8df077c6e6933f526fa518f7cd3f20
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-19 09:14:08 +0530

    add

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-// const title = document.getElementById("main-heading");
-// console.log(title);
+// selecting
+// selecting   id //
 
-// const list = document.getElementsByClassName("list-items");
-// console.log(list);
-
-// const lists = document.getElementsByTagName("li");
-// console.log(lists);
-
-// const heading = document.querySelector("div");
+// const heading = document.getElementById('heading');
 // console.log(heading);
+
+//selecting class //
+// const paragraph = document.getElementsByClassName("para");
+// console.log(paragraph);
+
+// selecting tag //
+
+// const link = document.getElementsByTagName("a");
+// console.log(link);
+
+// querySelector //
+// const heading = document.querySelector("#heading");
+// console.log(heading);
+
+// querySelectorAll //
 
 // const container = document.querySelectorAll("div");
 // console.log(container);
 
-// const heading = document.querySelector("#main-heading");
-// heading.style.color = "blue";
+// style //
+
+// const head = document.querySelector("#heading");
+// head.style.backgroundColor = "blue";
+
+// const paragraph = document.querySelector(".para");
+// paragraph.style.color = "red";
+
+// const list = document.querySelectorAll(".list-item");
+
+// for (i = 0; i < list.length; i++) {
+//     list[i].style.color = "green";
+// }
+
+
+// creating Elements //
+
+// const ul = document.querySelector("ul");
+// const li = document.createElement("li");
+// ul.append(li);
+
+// console.log(ul);
+
+// li.innerHTML = "created a li";
+// li.style.color="red";
+
+
+// modifying the text
+
+// innerText
+// textContent
+// innerHtml
+
+
+// modifying attributes and class
+
+
 
 
