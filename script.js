34b44969c9e33ceeb9b9d423e3b9b1684706225f
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-25 09:23:17 +0530

    add

--- script.js
+++ script.js
@@ -1,7 +1,92 @@
-// selecting the node and elements in dom tree for manipulation //
+// selecting the node / elements in dom tree for manipulation //
 
 // getElementById
-// getElementByClassName
-// getElementByTagName
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+// getElementsByClassName
+// const item = document.getElementsByClassName('list-items');
+// console.log(item);
+
+// getElementsByTagName
+// const tag = document.getElementsByTagName('li');
+// console.log(tag);
+
 // querySelector
+// select the first item which mention //
+// const container = document.querySelector('div');
+// console.log(container);
+
 // querySelectorAll
+// const whole = document.querySelectorAll('div');
+// console.log(whole);
+
+///////////////////////////////////////////////////////////////////
+
+// change the styling of the elements //
+
+// const heading = document.querySelector('#main-heading');
+// heading.style.color = 'yellow';
+
+
+// const list = document.querySelectorAll('list-items');
+// list.style.color ="red";
+// wont work
+// for multiple elements we have to loop
+
+const list = document.querySelectorAll('.list-items');
+// for (let i = 0; i < list.length; i++) {
+//     list[i].style.color = 'blue';
+// }
+
+/////////////////////////////////////////////////////////////////////
+
+// creating elements //
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+
+//  Adding element  //
+// ul.append(li);
+
+// modifying the text //
+// const firstItem = document.querySelector('.list-items');
+// console.log(firstItem.innerHTML);
+// console.log(firstItem.innerText);
+// console.log(firstItem.textContent);
+
+// li.innerText = 'monster';
+
+// modifying attributes and classes
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'yellow';
+// li.setAttribute('id', 'main-heading');
+
+// practice //
+
+// const title = document.querySelector('#heading');
+// title.style.color = "white";
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+ul.append(li);
+
+// li.innerText = 'done';
+
+// add css in external css //
+// li.setAttribute('id', 'heading');
+// li.removeAttribute('id');
+
+//    getAttribute //
+// const title = document.querySelector('#heading');
+// console.log(title.getAttribute('id'));
+
+// classList //
+li.innerText = 'kkkkkk';
+li.classList.add('list-items');
+// li.classList.remove('list-items');
+
+console.log(li.classList.contains('list-items'));
+
+// remove elements // 
+
+li.remove();
